fcacf316ce10a2fb0c7a1a8495ed4cf1b205688e
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-11-21 12:09:31 +0700

    Save

--- mf_improved.py
+++ mf_improved.py
@@ -5,6 +5,9 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
+from dataclasses import dataclass
+from typing import List
 from pytorch_lightning.loggers import WandbLogger
 
 from lit_data2 import  LitDataModule
@@ -20,8 +23,7 @@ class EmbeddingMulti(nn.Module):
 
     def forward(self, input):
         """ input: tensor of shape (batch_size, num_genres)"""
-        device = input.get_device()
-        result = torch.empty(size=(input.shape[0], self.embedding_dims), device=device)
+        result = torch.empty(size=(input.shape[0], self.embedding_dims), device=input.device)
         for i in range(input.shape[0]): # batch_size
             indices = torch.nonzero(input[i], as_tuple=True)[0]
             embeddings = [self.embedding(x) for _, x in enumerate(indices)]
@@ -70,9 +72,78 @@ class MatrixFactorization(nn.Module):
         bg = self.genre_bias(genre).flatten()
         return (Q*I).sum(-1) + (O*Q).sum(-1) + (I*G).sum(-1) + bq + bi + bo + bg
 
-    def inference(self, user_id):
-        Q = self.user_embedding(user_id)
-        bq = self.user_bias(user_id).flatten()
+@dataclass
+class MovieItem:
+    name: str
+    genres: List[str]
+
+    def __str__(self):
+        return f'Name: {self.name}. Genre: {", ".join(self.genres)}'
+
+class MatrixFactorizationInference:
+    def __init__(self, embedding_dims, num_users, num_items, num_occupations, num_genres):
+        self.df = pd.read_csv('csv_file/user_item_genre_occupation.csv')
+        self.model = MatrixFactorization(embedding_dims, num_users, num_items, num_occupations, num_genres)
+        self.idx2occ = [
+            'administrator',
+            'artist',
+            'doctor',
+            'educator',
+            'engineer',
+            'entertainment',
+            'executive',
+            'healthcare',
+            'homemaker',
+            'lawyer',
+            'librarian',
+            'marketing',
+            'none',
+            'other',
+            'programmer',
+            'retired',
+            'salesman',
+            'scientist',
+            'student',
+            'technician',
+            'writer',
+        ]
+        self.occ2idx = {occ: idx for idx, occ in enumerate(self.idx2occ)}
+        df_genre = pd.read_csv('ml-100k/u.genre', '|', names=['genre',  'index'])
+        genre_lst = df_genre["genre"].tolist()
+        df_item = pd.read_csv('ml-100k/u.item', '|', names=["item_id", "name", "year", "dummy", "url", *genre_lst])
+        df = df_item.drop(["year", "dummy", "url"], axis=1)
+        self.df_item = df
+
+    def search_for_occupation(self, user_id: int) -> int:
+        return self.occ2idx[self.df[self.df['user_id']==user_id]['occupation'].unique().item()]
+
+    def find_genre(self, item_id: int) -> int:
+        return torch.from_numpy(self.df[self.df['item_id'] == item_id].iloc[0, -(len(self.df.columns)-5):].to_numpy(dtype=np.int32))
+
+    def within_range(self, value: float, threshold: int) -> bool:
+        return abs(threshold - value) < 0.4
+
+    def id2movie(self, item_id: int) -> MovieItem:
+
+
+    def inference(self, user_id: int, max_num_items: int):
+        self.model.eval()
+        user_id -= 1
+        occ_id = self.search_for_occupation(user_id + 1)
+
+        user_id = torch.tensor([user_id])
+        occ_id = torch.tensor([occ_id])
+        rcd_ids = []
+        
+        for item_id in range(max_num_items):
+            item_id = torch.tensor([item_id])
+            genre_ids = self.find_genre(item_id + 1).unsqueeze(0)
+            rating = self.model(user_id, item_id, occ_id, genre_ids).item()
+            if self.within_range(range, 4):
+                rcd_ids.append(item_id + 1)
+        
+        for item_id in rcd_ids:
+
 
 
 class LitMF(LitModel):
@@ -99,8 +170,11 @@ def main(args):
     # model = LitMF(MatrixFactorization, embedding_dims=args.embedding_dims,  
     #     num_users=data.num_users, num_items=data.num_items, num_occupations=data.num_occupations, num_genres = data.num_genres, sparse=False
     #     )
-    print(data.num_users)
-    print(data.num_items)
+    # print(data.num_users)
+    # print(data.num_items)
+    inference = MatrixFactorizationInference(embedding_dims=args.embedding_dims,  
+        num_users=data.num_users, num_items=data.num_items, num_occupations=data.num_occupations, num_genres = data.num_genres)
+    inference.inference(1, 1000)
     
     # wandb_logger = WandbLogger(project="proj_dummy")
     # wandb_logger = WandbLogger(name="occ_genre_b", project="proj_recsys")
